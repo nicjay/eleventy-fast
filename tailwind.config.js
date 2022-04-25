92ba8f00a33d49915d1f2baa3e278f6c07d35ad6
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-04-24 19:18:06 -0700

    Code cleanup + styling (#14)
    * General code reorganization
    
    * Style home page
    
    * Dark mode tweaks
    
    * Move SVGs to own files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ module.exports = {
   content: [],
   darkMode: 'class',
   theme: {
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
